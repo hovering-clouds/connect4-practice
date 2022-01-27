f0b36406787032c99e59c9757f9c34a5e2c58caf
hovering-clouds <80081529+hovering-clouds@users.noreply.github.com>
2022-01-27 23:23:20 +0800

    Deploy to Heroku.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ logging.basicConfig(format="%(message)s", level=logging.DEBUG)
 
 import json
 from connect4 import Connect4,PLAYER1,PLAYER2
-
+import os,signal
 import secrets
 
 
@@ -144,9 +144,15 @@ async def play(websocket,game,player,connected):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
+        
 
 if __name__ == "__main__":
     asyncio.run(main())
